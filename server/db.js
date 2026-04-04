c45a84e70f77316773b0fdf0fbdda62f9d1ece47
krushnaa8552 <krushnaa8552@gmail.com>
2026-04-05 02:06:09 +0530

    preparation for deployment

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: 
-        PerformanceObserverEntryList.env.NODE_ENV === "production"
+        process.env.NODE_ENV === "production"
         ? { rejectUnauthorized: false }
         : false, 
 });
